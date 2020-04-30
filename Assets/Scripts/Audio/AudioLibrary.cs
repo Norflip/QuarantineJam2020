1e1eff882497595725439c82cee5dfebb7fa6f30
Filip Norgren <Norgren.filip@gmail.com>
2020-04-30 22:32:50 +0200

    Startup

---
+++ Assets/Scripts/Audio/AudioLibrary.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "New Audio Library")]
+public class AudioLibrary : ScriptableObject
+{
+    [NaughtyAttributes.ReorderableList]
+    public List<Sound> sounds = new List<Sound>();
+
+    [NaughtyAttributes.ReorderableList]
+    public List<SoundGroup> groups = new List<SoundGroup>();
+}
---
+++ Assets/Scripts/Audio/TestSound.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//TODO Kolla så den passar alla tests
+
+public class TestSound : MonoBehaviour
+{
+    public string key = "TEMP";
+
+    [NaughtyAttributes.Button]
+    void Run ()
+    {
+        if (Application.isPlaying)
+            AudioManager.Instance.PlayEffect(key);
+    }
+
+    [NaughtyAttributes.Button]
+    void Run3D ()
+    {
+        if (Application.isPlaying)
+            AudioManager.Instance.PlayEffect(key, transform.position);
+    }
+
+    [NaughtyAttributes.Button]
+    void RunAsGroup ()
+    {
+        if (Application.isPlaying)
+            AudioManager.Instance.PlayGroup(key, transform.position);
+    }
+
+    [NaughtyAttributes.Button]
+    void StopAll ()
+    {
+        if(Application.isPlaying)
+        {
+            AudioSource[] sources = GameObject.FindObjectsOfType<AudioSource>();
+            for (int i = 0; i < sources.Length; i++)
+                sources[i].Stop();
+        }
+    }
+}

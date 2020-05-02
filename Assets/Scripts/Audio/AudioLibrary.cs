fd83390915948a6fc374ad57db51762b489d06ff
Filip Norgren <Norgren.filip@gmail.com>
2020-05-02 02:23:35 +0200

    Update AudioLibrary.cs

--- Assets/Scripts/Audio/AudioLibrary.cs
+++ Assets/Scripts/Audio/AudioLibrary.cs
@@ -3,12 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 
+
 [CreateAssetMenu(fileName = "New Audio Library")]
 public class AudioLibrary : ScriptableObject
 {
-    [NaughtyAttributes.ReorderableList]
     public List<Sound> sounds = new List<Sound>();
-
-    [NaughtyAttributes.ReorderableList]
     public List<SoundGroup> groups = new List<SoundGroup>();
 }

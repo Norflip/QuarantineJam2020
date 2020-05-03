7fe33efedc14b0ed04b36d4baaea9ec3711e040e
Pudzz <55652759+Pudzz@users.noreply.github.com>
2020-05-03 02:18:39 +0200

    New models, shitbuttons.

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -5,6 +5,7 @@ public class Button : MonoBehaviour
     public void LoadScene(int i)
     {
         // fade out in 1 sec, wait 2 sec, fade in - in 1 sec
+        
         SceneController.LoadScene(i, 1, 2);
     }
 
